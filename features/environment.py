21e077f4b7ea6c45be8b400fbad03058a036c06d
Kate V <verkh.kate@gmail.com>
2022-07-08 23:21:08 -0500

    framework structure

---
+++ features/environment.py
@@ -0,0 +1,79 @@
+# import allure
+# from allure_commons.types import AttachmentType
+from app.application import Application
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.events import EventFiringWebDriver
+
+from support.logger import logger, MyListener
+
+bs_user = 'kateverkholantse_3iqWOB'
+bs_pw = 'YrsptBYT6MbzqgGxsnb7'
+
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+# allure serve test_results/
+
+def browser_init(context, test_name):
+    """
+    :param context: Behave context
+    :param test_name: scenario.name
+    """
+    context.driver = webdriver.Chrome()
+    #context.driver = webdriver.Firefox()
+    #context.driver = webdriver.Edge()
+    #context.driver = webdriver.Safari()
+
+    # HEADLESS MODE
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(chrome_options=options)
+    # OR context.driver = webdriver.Chrome(chrome_options=options, executable_path='')
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    ### for browerstack ###
+    # desired_cap = {
+    #     'browser': 'Chrome',
+    #     'os_version': '10',
+    #     'os': 'Windows',
+    #     'name': test_name
+    # }
+    # url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(5)
+    context.driver.wait = WebDriverWait(context.driver, 10)
+
+    context.app = Application(context.driver)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    # logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+    # logger.info(f'Started step: {step}')
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        logger.error(f'Step failed: {step}')
+        # print('\nStep failed: ', step)
+        # Mark test case as FAILED on BrowserStack:
+        # context.driver.execute_script(
+        #     'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Step failed"}}')
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
